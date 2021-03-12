b0eebb8597b09a5ac8127c9e871dfc0c248f8515
rixo <rixo@rixo.fr>
2021-03-12 23:25:31 +0100

    wip: remove dangerous transforms from normalizeNonCss

--- packages/vite-plugin-svelte/src/handleHotUpdate.ts
+++ packages/vite-plugin-svelte/src/handleHotUpdate.ts
@@ -14,7 +14,6 @@ export async function handleHotUpdate(
   cache: VitePluginSvelteCache
 ): Promise<ModuleNode[] | void> {
   const { read, server } = ctx
-  const { cssHash } = svelteRequest
   const cachedCompileData = cache.getCompileData(svelteRequest, false)
   if (!cachedCompileData) {
     // file hasn't been requested yet (e.g. async component)
@@ -39,7 +38,7 @@ export async function handleHotUpdate(
     affectedModules.add(cssModule)
   }
 
-  if (mainModule && jsChanged(cssHash, cachedCompileData, compileData)) {
+  if (mainModule && jsChanged(cachedCompileData, compileData)) {
     log.debug('handleHotUpdate js changed')
     affectedModules.add(mainModule)
   }
@@ -63,36 +62,24 @@ export async function handleHotUpdate(
   return result
 }
 
-function normalizeNonCss(code: string, cssHash: string) {
+function normalizeNonCss(code: string) {
   // trim HMR transform
   const indexHmrTransform = code.indexOf(
     'import * as ___SVELTE_HMR_HOT_API from'
   )
   if (indexHmrTransform !== -1) code = code.slice(0, indexHmrTransform)
-  // remove irrelevant bits
-  // NOTE the closer we're making our regexes to what we know of the compiler's
-  //      output, the more fragile we are, but the less we risk catching
-  //      similarly looking user's code
-  return (
-    code
-      // ignore css hashes in the code (that have changed, necessarily)
-      .replace(new RegExp('\\s*\\b' + cssHash + '\\b\\s*', 'g'), '')
-      // TODO this one might need to be a little more specific
-      .replace(/\s*attr_dev\([^,]+,\s*"class",\s*""\);?\s*/g, '')
-      // Svelte now adds locations in dev mode, code locations can change when
-      // CSS change, but we're unaffected (not real behaviour changes)
-      .replace(/\s*\badd_location\s*\([^)]*\)\s*;?/g, '')
-  )
+  // remove add_location -- changes but has no real world usage currently
+  return code.replace(/\s*\badd_location\s*\([^)]*\)\s*;?/g, '')
 }
 
 function cssChanged(prev: CompileData, next: CompileData) {
   return !isCodeEqual(prev.compiled.css?.code, next.compiled.css?.code)
 }
 
-function jsChanged(hash: string, prev: CompileData, next: CompileData) {
+function jsChanged(prev: CompileData, next: CompileData) {
   return !isCodeEqual(
-    normalizeNonCss(prev.compiled.js?.code, hash),
-    normalizeNonCss(next.compiled.js?.code, hash)
+    normalizeNonCss(prev.compiled.js?.code),
+    normalizeNonCss(next.compiled.js?.code)
   )
 }
 
