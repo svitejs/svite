20be19962aabdf26dc643cbfc78b26578daa95cc
Pebor <peborisbest@gmail.com>
2021-10-29 21:24:35 +0200

    make WindiCSS work again

--- packages/templates/windicss/vite.config.js
+++ packages/templates/windicss/vite.config.js
@@ -1,16 +1,13 @@
 import { svelte } from '@sveltejs/vite-plugin-svelte'
 import { defineConfig } from 'vite'
-import vitePluginWindicss from 'vite-plugin-windicss'
+import WindiCSS from 'vite-plugin-windicss'
 
 export default defineConfig(({ command, mode }) => {
   const isProduction = mode === 'production'
   return {
     plugins: [
-      // uses enforce: pre
       svelte({}),
-      vitePluginWindicss.default({
-        transformCSS: 'pre'
-      })
+      WindiCSS()
     ],
     build: {
       minify: isProduction
