edbcb2c6fd4736671c40ec8e7b3617ff91b8d431
Dominik G <dominik.goepel@gmx.de>
2021-10-30 16:39:23 +0400

    chore: update dependencies and adapt to changes (#23)
    * chore: update dependencies and adapt to changes
    
    * chore: update package to fix audit

--- packages/templates/windicss-ts/vite.config.js
+++ packages/templates/windicss-ts/vite.config.js
@@ -8,7 +8,7 @@ export default defineConfig(({ command, mode }) => {
     plugins: [
       // uses enforce: pre
       svelte(),
-      vitePluginWindicss.default({
+      vitePluginWindicss({
         transformCSS: 'pre'
       })
     ],
